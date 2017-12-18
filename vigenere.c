34ee25eafa9139b059ebb580b8d0e643d8907df3
Zi Nguyen <zeexnguyen@gmail.com>
2017-12-18 14:30:55 +0000

    add command-line arg validation and check if chars in p are alphabetic

--- vigenere.c
+++ vigenere.c
@@ -1,9 +1,33 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <string.h>
 
 int main(argc, argv[]) {
+    // check condition of user's input
     if (argc !== 2 || (isalpha(argv[1]) === false)) {
         printf("./vigenere k\n");
-        return 0;
+        return 1;
     }
+
+    string k = argv[1];
+    // prompt user for plaintext
+    printf("plaintext: ");
+    string p = get_string();
+
+    if(p != NULL) {
+        for(var i = 0, n = strlen(p); i < n; i++) {
+            // check if p[i] is alphabetic
+            //if not, print unchanged
+            if (isalpha(p[i]) != true) {
+                printf("%c", p[i]);
+            } else {
+                //encipher!
+            }
+
+        }
+    }
+
+
+    printf("\n");
+    return 0;
 }
