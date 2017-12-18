c92f5c871e513ffcfb0c81f066ffcb35fab68b0f
Zi Nguyen <zeexnguyen@gmail.com>
2017-12-18 16:43:06 +0000

    make it work

--- vigenere.c
+++ vigenere.c
@@ -1,33 +1,67 @@
 #include <stdio.h>
 #include <cs50.h>
 #include <string.h>
+#include <ctype.h>
 
-int main(argc, argv[]) {
-    // check condition of user's input
-    if (argc !== 2 || (isalpha(argv[1]) === false)) {
-        printf("./vigenere k\n");
+int main(int argc, string argv[]) {
+    // check if user includes keyword
+    if (argc != 2) {
+        printf("Usage: ./vigenere keyword\n");
         return 1;
     }
-
+    //check if keyword is alphabetic
     string k = argv[1];
-    // prompt user for plaintext
-    printf("plaintext: ");
-    string p = get_string();
-
-    if(p != NULL) {
-        for(var i = 0, n = strlen(p); i < n; i++) {
-            // check if p[i] is alphabetic
-            //if not, print unchanged
-            if (isalpha(p[i]) != true) {
-                printf("%c", p[i]);
-            } else {
-                //encipher!
-            }
-
+    for (int a = 0, b = strlen(k); a < b; a++) {
+        if (!isalpha(k[a])) {
+            printf("Usage: ./vigenere keyword\n");
+            return 1;
         }
     }
 
+    // if all is well, prompt user for plaintext
+    printf("plaintext: ");
+    string p = get_string();
 
+    //encipher!
+    if (p != NULL) {
+        printf("ciphertext: ");
+        int shift;
+        for (int i = 0, n = strlen(p), j = 0, m = strlen(k); i < n && j < m; i++) {
+            if (isalpha(p[i])) {
+                if (isupper(p[i])) {
+                    int textpos = p[i] - 'A';
+
+                    if (isupper(k[j])) {
+                        shift = k[j] - 'A';
+                    } else {
+                        shift = k[j] - 'a';
+                    }
+
+                    int cipherpos = (textpos + shift) % 26;
+                    int c = cipherpos + 'A';
+                    printf("%c", (char) c);
+                    j = (j + 1) % m;
+                }
+                //if lowercase
+                if (islower(p[i])) {
+                    int textpos = p[i] - 'a';
+
+                    if (isupper(k[j])) {
+                        shift = k[j] - 'A';
+                    } else {
+                        shift = k[j] - 'a';
+                    }
+
+                    int cipherpos = (textpos + shift) % 26;
+                    int c = cipherpos + 'a';
+                    printf("%c", (char) c);
+                    j = (j + 1) % m;
+                }
+            } else {
+                printf("%c", p[i]);
+            }
+        }
+    }
     printf("\n");
     return 0;
 }
